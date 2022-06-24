7f116a8facf51ca797828a47bdd4dd8fe841f8ac
Abdülmuttalib GÜLER <64988496+TalibGuler@users.noreply.github.com>
2022-06-24 10:02:31 +0300

    24-06-2022

---
+++ CvProject/Controllers/DefaultController.cs
@@ -0,0 +1,38 @@
+﻿using CvProject.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CvProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DbCvEntities db = new DbCvEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.about.ToList();
+            return View(degerler);
+        }
+
+        public PartialViewResult Deneyim()
+        {
+            var deneyimler = db.experiences.ToList();
+            return PartialView(deneyimler);
+        }
+
+        public PartialViewResult Egitimlerim()
+        {
+            var egitimlerim = db.trainings.ToList();
+            return PartialView(egitimlerim);
+        }
+
+        public PartialViewResult Skills()
+        {
+            var skills = db.skills.ToList();
+            return PartialView(skills);
+        }
+    }
+}
